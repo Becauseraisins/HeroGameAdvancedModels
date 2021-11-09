88f8250c207e152d200d72ec4dc317f52b1590f4
Becauseraisins <79813812+Becauseraisins@users.noreply.github.com>
2021-11-09 16:16:05 +1100

    Villain test

--- HeroGameAdvancedTests/VillainTests.cs
+++ HeroGameAdvancedTests/VillainTests.cs
@@ -4,11 +4,17 @@ using HeroGameAdvancedLib;
 namespace HeroGameAdvancedTests
 {        
     public class VillainTests
-    {
-        [Fact]
-        public void Test1()
+    {   Villain villain1 = new Villain(1,"v1",100);
+        [Theory]
+        [InlineData(100,0)]
+        [InlineData(99999,0)]
+        [InlineData(69,31)]
+        [InlineData(1,99)]
+        [InlineData(-1,101)]
+        public void Test1(int input, int expectedHp)
         {
-
+            villain1.Damage(input);
+            Assert.Equal(expectedHp,villain1.Hitpoints);
         }
     }
 
