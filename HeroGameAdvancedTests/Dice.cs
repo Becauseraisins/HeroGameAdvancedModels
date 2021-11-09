cd04a0a059b1a1fad30538b5cb8975f4d1c4a348
Becauseraisins <79813812+Becauseraisins@users.noreply.github.com>
2021-11-09 15:59:24 +1100

    dice tests

--- HeroGameAdvancedTests/Dice.cs
+++ HeroGameAdvancedTests/Dice.cs
@@ -5,12 +5,15 @@ namespace HeroGameAdvancedTests
 {        
     public class DiceTests
     {   Dice dice1;
-        DiceTests(){}
         [Theory]
-        [InlineData(1,100)]
+        [InlineData(1,1)]
+        [InlineData(10,10)]
+        [InlineData(10,1)]
+        [InlineData(100,1000)]
         public void Test1(int mindamage, int maxdamage)
-        {
-
+        {   this.dice1 = new Dice(mindamage,maxdamage);
+            Assert.True( dice1.Roll() >= mindamage);
+            Assert.True(dice1.Roll() <= maxdamage);
         }   
     }
 
